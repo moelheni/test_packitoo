2f73ae51eac0caa95db5703e8e1de40ea51d5524
Elheni Mokhles <emokhles@figarocms.fr>
2019-06-21 20:08:54 +0200

    fix typo

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  test 'body sould be saved' do
+  test 'body should be saved' do
     article = Article.create title: 'MOCK ARTICLE', text: 'text'
     last_article = Article.all.last
     assert_equal 'text', last_article.text
